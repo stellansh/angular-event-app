ac7f6e7dfeb4658df70922d19cce6bf44e64f058
stellansh <stellanshuti2@gmail.com>
2020-11-18 14:34:57 -0500

    3 files modified, 3 added, 3 deleted

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -10,6 +10,7 @@ export class HomeComponent implements OnInit, OnDestroy {
     currentUser: User;
     currentUserSubscription: Subscription;
     users: User[] = [];
+   
 
     constructor(
         private authenticationService: AuthenticationService,
